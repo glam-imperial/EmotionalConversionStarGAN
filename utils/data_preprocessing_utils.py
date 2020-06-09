8090701033677c6e99b15ddb63d33a1ffe1b3a65
Max Elliott <maxelliott456@outlook.com>
2020-06-09 22:25:48 +0100

    Debug preprocessing scripts and restructured the whole directory to be more readable.

---
+++ utils/data_preprocessing_utils.py
@@ -0,0 +1,334 @@
+'''
+data_preprocessing2.py
+
+Author - Max Elliott
+
+Functions for pre-processing the IEMOCAP dataset. Can make mel-specs, WORLD
+features, and labels for each audio clip.
+'''
+
+import torch
+
+from utils import audio_utils
+
+import numpy as np
+import os
+from librosa.util import find_files
+
+
+def get_speaker_from_filename(filename):
+    code = filename[4] + filename[-8]
+
+    conversion = {'1F':0, '1M':1, '2F':2, '2M':3, '3F':4, '3M':5, '4F':6, '4M':7,
+                    '5F': 8, '5M':9}
+
+    label = conversion[code]
+
+    return label
+
+
+def get_emotion_from_label(category):
+
+    if category == 'xxx' or category =='dis' or category =='fea' or category == 'oth':
+        return -1
+    if category == 'exc' or category == 'fru' or category == 'sur':
+        return -1
+
+    conversion = {'ang':0, 'sad':1, 'hap':2, 'neu':3}
+
+    label = conversion[category]
+
+    return label
+
+
+def getOneHot(label, n_labels):
+
+    onehot = np.zeros(n_labels)
+    onehot[label] = 1
+
+    return onehot
+
+
+def cont2list(cont, binned = False):
+
+    list = [0,0,0]
+    list[0] = float(cont[1:6])
+    list[1] = float(cont[9:14])
+    list[2] = float(cont[17:22])
+
+    #Option to make the values discrete: low(0), med(1) or high(2)
+    if binned:
+        for i, val in enumerate(list):
+            if val <= 2:
+                list[i] = 0
+            elif val < 4:
+                list[i] = 1
+            else:
+                list[i] = 2
+        return list
+    else:
+        return list
+
+
+def concatenate_labels(emo, speaker, dims, dims_dis):
+
+    all_labels = torch.zeros(8)
+    # print(all_labels)
+
+    # for i, row in enumerate(all_labels):
+    all_labels[0] = emo
+    all_labels[1] = speaker
+    all_labels[2] = dims[0]
+    all_labels[3] = dims[1]
+    all_labels[4] = dims[2]
+    all_labels[5] = dims_dis[0]
+    all_labels[6] = dims_dis[1]
+    all_labels[7] = dims_dis[2]
+
+
+    return all_labels
+
+
+def get_wav_and_labels(filename, data_dir):
+
+    # folder = filename[:-9]
+    wav_path = os.path.join(data_dir, "audio", filename)
+    label_path = os.path.join(data_dir, "annotations", filename[:-9] + ".txt")
+
+    with open(label_path, 'r') as label_file:
+
+        category = ""
+        dimensions = ""
+        speaker = ""
+
+        for row in label_file:
+            if row[0] == '[':
+                split = row.split("\t")
+                if split[1] == filename[:-4]:
+                    category = get_emotion_from_label(split[2])
+                    dimensions = cont2list(split[3])
+                    dimensions_dis = cont2list(split[3], binned = True)
+                    speaker = get_speaker_from_filename(filename)
+
+
+    audio = audio_utils.load_wav(wav_path)
+    audio = np.array(audio, dtype = np.float32)
+    labels = concatenate_labels(category, speaker, dimensions, dimensions_dis)
+
+    return audio, labels
+
+
+def get_samples_and_labels(filename, config):
+
+    # config = yaml.load(open('./config.yaml', 'r'))
+
+    wav_path = config['data']['sample_set_dir'] + "/" + filename
+    folder = filename[:-9]
+    label_path = config['data']['dataset_dir'] + "/Annotations/" + folder + ".txt"
+
+    with open(label_path, 'r') as label_file:
+
+        category = ""
+        dimensions = ""
+        speaker = ""
+
+        for row in label_file:
+            if row[0] == '[':
+                split = row.split("\t")
+                if split[1] == filename[:-4]:
+                    category = get_emotion_from_label(split[2])
+                    dimensions = cont2list(split[3])
+                    dimensions_dis = cont2list(split[3], binned = True)
+                    speaker = get_speaker_from_filename(filename)
+
+
+    audio = audio_utils.load_wav(wav_path)
+    audio = np.array(audio, dtype = np.float32)
+    labels = concatenate_labels(category, speaker, dimensions, dimensions_dis)
+
+    return audio, labels
+
+
+def get_filenames(data_dir):
+
+    files = find_files(data_dir, ext = 'wav')
+    filenames = []
+
+    for f in files:
+        f = os.path.basename(f)[:-4]
+        filenames.append(f)
+
+    return filenames
+
+
+if __name__ == '__main__':
+
+    min_length = 0 # actual is 59
+    max_length = 688
+
+    data_dir = '/Users/Max/MScProject/data'
+    annotations_dir = os.path.join(data_dir, "audio")
+    files = find_files(annotations_dir, ext = 'wav')
+
+    filenames = []
+    for f in files:
+        f = os.path.basename(f)
+        filenames.append(f)
+
+
+
+    ############################################
+    #      Code for making mels and labels     #
+    ############################################
+    i = 0
+    found = 0
+    lengths = []
+    longest_length = 0
+    longest_name = ""
+    for f in filenames:
+        if i >10000:
+            print(f)
+        wav, labels = get_wav_and_labels(f, data_dir)
+        # mel = audio_utils.wav2melspectrogram(wav)
+        labels = np.array(labels)
+        if labels[0] in range(0,4) and f[0:3] == 'Ses':
+
+            length = wav.shape[0]/16000.
+            lengths.append(length)
+            # np.save(data_dir + "/mels/" + f[:-4] + ".npy", mel)
+            # np.save(data_dir + "/labels/" + f[:-4] + ".npy", labels)
+            found += 1
+
+            if length > longest_length:
+                longest_length = length
+                longest_name = f
+
+        i += 1
+        if i % 100 == 0:
+            print(i, " complete.")
+            print(found, "found.")
+
+    print(found, "found.")
+    print(f"longest + {longest_name}")
+
+    lengths.sort()
+    lengths = lengths[:int(len(lengths)*0.9)]
+    print("Total seconds =", np.sum(lengths))
+
+    # n, bins, patches = plt.hist(lengths, bins = 32)
+    # plt.xlabel('Sequence length / seconds')
+    # plt.xlim(0, 32)
+    # plt.ylabel('Count')
+    # plt.title(r'Histogram of sequence lengths for 4 emotional categories')
+    # plt.show()
+
+    ############################################
+    #      Loop through mels for analysis      #
+    ############################################
+    # files = find_files(data_dir + "/mels", ext = 'npy')
+    # lengths = []
+    # for f in files:
+    #
+    #     mel = np.load(f)
+    #     lengths.append(mel.shape[1])
+    #     # print(mel.shape)
+    #
+    # n, bins, patches = plt.hist(lengths, bins = 22)
+    # plt.xlabel('Sequence length')
+    # plt.ylabel('Count')
+    # plt.title(r'New histogram of sequence lengths for 4 emotional categories')
+    # plt.show()
+
+    ############################################
+    #     Loop through labels for analysis     #
+    ############################################
+    # files = find_files(data_dir + "/labels", ext = 'npy')
+    # category_counts = np.zeros((4))
+    # speaker_counts = np.zeros((10))
+    # for f in files:
+    #
+    #     labels = np.load(f)
+    #     cat = int(labels[0])
+    #     speaker = int(labels[1])
+    #     category_counts[cat] += 1
+    #     speaker_counts[speaker] += 1
+    #
+    # print(category_counts)
+    # print(speaker_counts)
+    # #### RESULTS ####
+    # # [ 549.  890.  996. 1605.] 4040 total
+    # # [416. 425. 353. 364. 448. 480. 342. 370. 473. 369.]
+    # #### # # # # ####
+    #
+    # def make_autopct(values):
+    #
+    #     def my_autopct(pct):
+    #         total = sum(values)
+    #         val = int(round(pct*total/100.0))
+    #         return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    #
+    #     return my_autopct
+    #
+    # plt.pie(category_counts, labels = ['Happy','Sad','Angry','Neutral'],
+    #         autopct =make_autopct(category_counts), shadow=False)
+    # plt.show()
+    #
+    # plt.pie(speaker_counts, labels = ['Ses01F','Ses01M','Ses02F','Ses02M','Ses03F',
+    #                                 'Ses03M','Ses04F','Ses04M','Ses05F','Ses05M'],
+    #         autopct ='%1.1f%%', shadow=False)
+    # plt.show()
+
+    # 1.34591066837310
+
+
+    ############################################
+    #   Finding min and max intensity of mels  #
+    ############################################
+    # i = 0
+    # mels_made = 0
+    # mel_lengths = []
+    #
+    # max_intensity = 0
+    # min_intensity = 99999999
+    #
+    # for f in filenames:
+    #
+    #     wav, labels = get_wav_and_labels(f, data_dir)
+    #     mel = audio_utils.wav2melspectrogram(wav)
+    #     labels = np.array(labels)
+    #     if labels[0] != -1:
+    #
+    #         # mel_lengths.append(mel.shape[1])
+    #         max_val = np.max(mel)
+    #         min_val = np.min(mel)
+    #
+    #         if max_val > max_intensity:
+    #             max_intensity = max_val
+    #         if min_val < min_intensity:
+    #             min_intensity = min_val
+    #         mels_made += 1
+    #
+    #     i += 1
+    #     if i % 100 == 0:
+    #         # print(mel_lengths[mels_made-1])
+    #         print(mel[:, 45])
+    #         print(max_intensity, ", ", min_intensity)
+    #         print(i, " complete.")
+    #         print(mels_made, "mels made.")
+    #
+    # print("max = {}".format(max_intensity))
+    # print("min = {}".format(min_intensity))
+    #
+    # np.save('./stats/all_mel_lengths', np.array(mel_lengths))
+    #
+    # n, bins, patches = plt.hist(mel_lengths, bins = 22)
+    # plt.xlabel('Sequence length')
+    # plt.ylabel('Count')
+    # plt.title(r'Histogram of sequence lengths for 4 emotional categories')
+    # plt.show()
+    #
+    # mel_lengths = sorted(mel_lengths)
+    # print(mel_lengths[0:30])
+    # split_index = int(len(mel_lengths)*0.9)
+    # print(mel_lengths[split_index])  # IS MAX LENGTH OF mels
+    # print(mel_lengths[0])  # IS MIN LENGTH OF mels
---
+++ utils/preprocess_world.py
@@ -0,0 +1,227 @@
+'''
+preprocess_world.py
+
+Author - Max Elliott
+
+Functions for extracting WORLD features form wav files. ALso for initial
+pre-processing of data.
+'''
+
+import librosa
+from librosa.util import find_files
+import numpy as np
+import os
+import pyworld
+# import pyworld as pw
+from pyworld import decode_spectral_envelope, synthesize
+import glob
+# from utility import *
+import argparse
+from datetime import datetime
+import pickle
+
+from utils import audio_utils
+import utils.data_preprocessing_utils as pp
+
+FEATURE_DIM = 36
+SAMPLE_RATE = 16000
+FRAMES = 512
+FFTSIZE = 1024
+
+def world_features(wav, sr, fft_size, dim):
+    f0, timeaxis = pyworld.harvest(wav, sr)
+    sp = pyworld.cheaptrick(wav, f0, timeaxis, sr,fft_size=fft_size)
+    ap = pyworld.d4c(wav, f0, timeaxis, sr, fft_size=fft_size)
+    coded_sp = pyworld.code_spectral_envelope(sp, sr, dim)
+
+    return f0, timeaxis, sp, ap, coded_sp
+
+def cal_mcep(wav, sr=SAMPLE_RATE, dim=FEATURE_DIM, fft_size=FFTSIZE):
+    '''cal mcep given wav singnal
+        the frame_period used only for pad_wav_to_get_fixed_frames
+    '''
+    f0, timeaxis, sp, ap, coded_sp = world_features(wav, sr, fft_size, dim)
+
+    if audio_utils.hp.normalise_mels:
+        coded_sp = audio_utils._normalise_coded_sp(coded_sp)
+        # print("Normalised")
+
+    coded_sp = coded_sp.T # dim x n
+
+    return f0, ap, sp, coded_sp
+
+def get_f0_stats(f0s):
+    log_f0s_concatenated = np.ma.log(np.concatenate(f0s))
+    log_F0s_no0 = []
+    # for v in log_f0s_concatenated:
+    #     if v != 0:
+    #         log_F0s_no0.append(v)
+    log_f0s_mean = log_f0s_concatenated.mean()
+    log_f0s_std = np.var(log_f0s_concatenated)
+
+
+    return log_f0s_mean, log_f0s_std
+
+if __name__ == "__main__":
+
+
+    ###########################################
+    #       WORLD features testing code       #
+    ###########################################
+    data_dir = '../data/audio/'
+    sample = data_dir + 'Ses01F_impro01_F000.wav'
+    sr = 16000
+    # wav = librosa.load(sample, sr=SAMPLE_RATE, mono=True, dtype=np.float64)[0]
+    # f0, ap, sp, coded_sp = cal_mcep(wav)
+    # mel = audio_utils.wav2melspectrogram(wav).transpose()
+    # print(coded_sp.shape)
+    # print(mel.shape)
+    # f0 = np.ascontiguousarray(f0[200:], dtype=np.float64)
+    # ap = np.ascontiguousarray(ap[200:,:], dtype=np.float64)
+    # coded_sp = np.ascontiguousarray(coded_sp.T[200:,:], dtype=np.float64)
+    # audio_utils.save_world_wav([f0,ap,sp,coded_sp], 'tests', 'crop_test.wav')
+
+    # decoded_sp = decode_spectral_envelope(coded_sp, SAMPLE_RATE, fft_size=FFTSIZE)
+    # f0_converted = norm.pitch_conversion(f0, speaker, target)
+    # wav2 = synthesize(f0, decoded_sp, ap, SAMPLE_RATE)
+    # audio_utils.save_wav(wav2, './samples/worldwav.wav')
+
+    min_length = 0 # actual is 59
+    max_length = 1719
+    #
+    data_dir = '/Users/Max/MScProject/data'
+    annotations_dir = "/Users/Max/MScProject/data/labels"
+    files = find_files(annotations_dir, ext = 'npy')
+    print(len(files))
+    # # print(f0.shape) # (len,)
+    # # print(ap.shape) # (len, 513) assum n_fft//2+1
+    # # print(coded_sp.shape) # (len, 36)
+    filenames = []
+    for f in files:
+        f = os.path.basename(f)[:-4] + ".wav"
+        filenames.append(f)
+    #
+    # print(filenames[0:3])
+    # i = 0
+    # mels_made = 0
+    # for f in filenames:
+    #
+    #     wav, labels = pp.get_wav_and_labels(f, data_dir)
+    #     labels = np.array(labels)
+    #
+    #     if labels[0] != -1:
+    #
+    #         np.save(data_dir + "/labels/" + f[:-4] + ".npy", labels)
+    #         mels_made += 1
+    #
+    #     i += 1
+    #     if i % 100 == 0:
+    #         print(i, " complete.")
+    #         print(mels_made, "labels made.")
+
+
+    ############################################
+    #      Code for making mels and labels     #
+    ############################################
+    # i = 0
+    # worlds_made = 0
+    # lengths = []
+    # for f in filenames:
+    #
+    #     wav, labels = pp.get_wav_and_labels(f, data_dir)
+    #     wav = np.array(wav, dtype = np.float64)
+    #     labels = np.array(labels)
+    #     #
+    #     f0, ap, sp, coded_sp = cal_mcep(wav)
+    #     # coded_sp = np.load(f)
+    #
+    #     if labels[0] != -1 and coded_sp.shape[1] < max_length:
+    #
+    #         # lengths.append(coded_sp.shape[1])
+    #         # np.save(data_dir + "/world/" + f[:-4] + ".npy", coded_sp)
+    #         # np.save(data_dir + "/labels/" + f[:-4] + ".npy", labels)
+    #         np.save(data_dir + "/f0/" + f[:-4] + ".npy", f0)
+    #
+    #         # os.remove(f)
+    #         # print("Removed ", os.path.basename(f), " . Length = ", coded_sp.shape[1])
+    #         worlds_made += 1
+    #
+    #     i += 1
+    #     if i % 10 == 0:
+    #         print(i, " complete.")
+    #         print(worlds_made, "worlds made.")
+
+    # lengths.sort()
+    # cutoff = int(len(lengths)*0.9)
+    # print(lengths[0:100])
+    # print("Cutoff length is ", lengths[cutoff])
+    #
+    #
+    # n, bins, patches = plt.hist(lengths, bins = 22)
+    # plt.xlabel('Sequence length')
+    # plt.ylabel('Count')
+    # plt.title(r'New histogram of sequence lengths for 4 emotional categories')
+    # plt.show()
+    ############################################
+    #            Generate f0_dict              #
+    ############################################
+    emo_stats = {}
+    for e in range(0,4):
+        spk_dict = {}
+        for s in range(0,10):
+            f0s = []
+            for f in filenames:
+                wav, labels = pp.get_wav_and_labels(f, data_dir)
+                wav = np.array(wav, dtype = np.float64)
+                labels = np.array(labels)
+                if labels[0] == e and labels[1] == s:
+                    f0_dir = data_dir +"/f0/" + f[:-4] + ".npy"
+                    f0 = np.load(f0_dir)
+                    f0s.append(f0)
+
+            log_f0_mean, f0_std = get_f0_stats(f0s)
+            spk_dict[s] = (log_f0_mean, f0_std)
+            print(f"Done emotion {e}, speaker {s}.")
+        emo_stats[e] = spk_dict
+
+    with open('f0_dict.pkl', 'wb') as f:
+        pickle.dump(emo_stats, f, pickle.HIGHEST_PROTOCOL)
+
+    for tag, val in emo_stats.items():
+        print(f'Emotion {tag} stats:')
+        for tag2, val2 in val.items():
+            print(f'{tag2} = {val2[0]}, {val2[1]}')
+
+
+    ############################################
+    #  Finding min and max intensity of mpecs  #
+    ############################################
+    # i = 0
+    #
+    # max_intensity = -9999999
+    # min_intensity = 99999999
+    #
+    # for f in files:
+    #
+    #     coded_sp = np.load(f)
+    #     # coded_sp = audio_utils._normalise_coded_sp(coded_sp)
+    #     # np.save(f, coded_sp)
+    #     # mel_lengths.append(mel.shape[1])
+    #     max_val = np.max(coded_sp)
+    #     min_val = np.min(coded_sp)
+    #
+    #     if max_val > max_intensity:
+    #         max_intensity = max_val
+    #     if min_val < min_intensity:
+    #         min_intensity = min_val
+    #
+    #
+    #     i += 1
+    #     if i % 100 == 0:
+    #         # print(mel_lengths[mels_made-1])
+    #         # print(mel[:, 45])
+    #         print(max_intensity, ", ", min_intensity)
+    #         print(i, " complete.")
+    #
+    # print("max = {}".format(max_intensity))
+    # print("min = {}".format(min_intensity))
